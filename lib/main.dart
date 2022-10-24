25fafc85abeeed10cd611c875b021e0a4a9de316
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2022-10-24 15:56:05 +0300

    Select also on name click, cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
@@ -9,21 +8,11 @@ void main() {
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const StartupNameList(title: 'Startup Name Generator'),
@@ -33,15 +22,6 @@ class App extends StatelessWidget {
 
 class StartupNameList extends StatefulWidget {
   const StartupNameList({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,53 +36,53 @@ class _StartupNameListState extends State<StartupNameList> {
   final _startupNames = <WordPair>[];
 
   List<WordPair> _getFilteredStartupNames() {
-    if(showOnlySelected) {
-      return _startupNames.where((wordpair) => namesSelected.containsKey(wordpair)).toList();
+    if (showOnlySelected) {
+      return _startupNames
+          .where((wordpair) => namesSelected.containsKey(wordpair) && (namesSelected[wordpair] ?? false))
+          .toList();
     }
     return _startupNames;
   }
 
+  void _generateMoreStartupNames() {
+    _startupNames.addAll(generateWordPairs().take(10));
+  }
+
+  void _toggleNameSelection(index) {
+    var startupNames = _getFilteredStartupNames();
+    setState(() {
+      namesSelected[startupNames[index]] =
+          !(namesSelected[startupNames[index]] ?? false);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    print(_getFilteredStartupNames().length);
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Scrollbar(
           child: ListView.builder(
-        restorationId: 'list_view',
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        itemCount: showOnlySelected ? _getFilteredStartupNames().length : null,
-        itemBuilder:(context, index) {
-          var names = _getFilteredStartupNames();
-          print('index  $index');
-          var len = names.length;
-          print('names in list $len');
-          // Showing only selected causes index to overflow due to collapsed items, so we disable it here
-          if(!showOnlySelected && index >= _startupNames.length) {
-            // Generate more names when reaching end
-             _startupNames.addAll(generateWordPairs().take(10));
-          }
-          return ListTile(
-                  leading: Checkbox(
-                      value: namesSelected[names[index]] ?? false,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          namesSelected[names[index]] = value ?? false;
-                        });
-                      }),
-                  title: Text(names[index].asPascalCase));
-        }
-      )),
+              restorationId: 'list_view',
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              // Set strict item count when showing only selected to prevent from auto-generating new words
+              itemCount:
+                  showOnlySelected ? _getFilteredStartupNames().length : null,
+              itemBuilder: (context, index) {
+                var startupNames = _getFilteredStartupNames();
+                if (index >= startupNames.length) {
+                  // Generate more names when reaching end
+                  _generateMoreStartupNames();
+                }
+                return ListTile(
+                    onTap: () => _toggleNameSelection(index),
+                    leading: Checkbox(
+                      value: namesSelected[startupNames[index]] ?? false,
+                      onChanged: (value) => _toggleNameSelection(index),
+                    ),
+                    title: Text(startupNames[index].asPascalCase));
+              })),
       floatingActionButton: FloatingActionButton(
           onPressed: () => setState(() {
                 showOnlySelected = !showOnlySelected;
@@ -110,10 +90,7 @@ class _StartupNameListState extends State<StartupNameList> {
           tooltip: 'Show selected',
           child: showOnlySelected
               ? const Icon(Icons.filter_alt_off)
-              : const Icon(Icons
-                  .filter_alt)), // This trailing comma makes auto-formatting nicer for build methods.
+              : const Icon(Icons.filter_alt)),
     );
   }
 }
-
-String nextRandStartupName() => WordPair.random().asPascalCase;
